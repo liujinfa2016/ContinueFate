8ebe6f865e8ad100dc47d83bbc9f34dfb042804b
px <675211749@qq.com>
2016-04-17 18:25:29 +0800

    更新TabBar

--- ContinueFate/ContinueFate/TabBarViewController.m
+++ ContinueFate/ContinueFate/TabBarViewController.m
@@ -7,33 +7,84 @@
 //
 
 #import "TabBarViewController.h"
+#import "XZMTabbarExtension.h"
+#import "ViewController.h"
 
-@interface TabBarViewController ()
-
-
+@interface TabBarViewController ()<XZMTabbarExtensionDelegate>
+@property (nonatomic, strong)NSMutableArray *itemArray;
 @end
 
 @implementation TabBarViewController
 
+- (NSMutableArray *)itemArray
+{
+    if (_itemArray == nil) {
+        _itemArray = [NSMutableArray array];
+    }
+    return _itemArray;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-
+    ViewController *home = [Utilities getStoryboardInstanceByIdentity:@"Main" byIdentity:@"Home"];
+    [self tabBarChildViewController:home norImage:[UIImage imageNamed:@"tabBar_0"] selImage:[UIImage imageNamed:@"tabBar_0_on"]];
+    ViewController *article = [Utilities getStoryboardInstanceByIdentity:@"Article" byIdentity:@"Article"];
+    [self tabBarChildViewController:article norImage:[UIImage imageNamed:@"tabBar_1"] selImage:[UIImage imageNamed:@"tabBar_1_on"]];
+    ViewController *question = [Utilities getStoryboardInstanceByIdentity:@"Question" byIdentity:@"Question"];
+    [self tabBarChildViewController:question norImage:[UIImage imageNamed:@"tabBar_2"] selImage:[UIImage imageNamed:@"tabBar_2_on"]];
+    ViewController *consulting = [Utilities getStoryboardInstanceByIdentity:@"Consulting" byIdentity:@"Consulting"];
+    [self tabBarChildViewController:consulting norImage:[UIImage imageNamed:@"tabBar_3"] selImage:[UIImage imageNamed:@"tabBar_3_on"]];
+    [self setTatBar];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+/** View激将显示时 */
+- (void)viewWillAppear:(BOOL)animated
+{   [super viewWillAppear:animated];
+    for (UIView *childView in self.tabBar.subviews) {
+        if (![childView isKindOfClass:[XZMTabbarExtension class]]) {
+            [childView removeFromSuperview];
+        }
+    }
 }
-*/
+
+- (void)setTatBar
+{
+    /** 创建自定义tabbar */
+    XZMTabbarExtension *tabBar = [[XZMTabbarExtension alloc] init];
+    tabBar.backgroundColor = [UIColor whiteColor];
+    tabBar.frame = self.tabBar.bounds;
+    /** 传递模型数组 */
+    tabBar.items = self.itemArray;
+    //    [tabBar xzm_setShadeItemBackgroundColor:[UIColor redColor]];
+    /** 设置代理 */
+    tabBar.delegate = self;
+    
+    [self.tabBar addSubview:tabBar];
+}
+
+- (void)tabBarChildViewController:(UIViewController *)vc norImage:(UIImage *)norImage selImage:(UIImage *)selImage
+{
+    /** 创建导航控制器 */
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
+    
+    /** 创建模型 */
+    UITabBarItem *tabBarItem = [[UITabBarItem alloc] init];
+    tabBarItem.image = norImage;
+    tabBarItem.selectedImage = selImage;
+    /** 添加到模型数组 */
+    [self.itemArray addObject:tabBarItem];
+    [self addChildViewController:nav];
+}
+
+/** 代理方法 */
+- (void)xzm_tabBar:(XZMTabbarExtension *)tabBar didSelectItem:(NSInteger)index{
+    self.selectedIndex = index;
+}
 
 @end
+
