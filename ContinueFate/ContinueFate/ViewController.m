0ea9f1543fd214748f3f81d9f90b1437f508acd6
px <675211749@qq.com>
2016-04-17 17:10:08 +0800

    MBProgressHUD扩展封装

--- ContinueFate/ContinueFate/ViewController.m
+++ ContinueFate/ContinueFate/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "MBProgressHUD+NJ.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //显示加载
+    [MBProgressHUD showMessage:@"正在加载" toView:self.view];
+    //加载完成
+    [MBProgressHUD hideHUDForView:self.view];
 }
 
 - (void)didReceiveMemoryWarning {
