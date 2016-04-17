dddf6435403d5d35bbd5a586989c3bdd1271a738
demon <237594006@qq.com>
2016-04-17 20:14:55 +0800

    更新

--- ContinueFate/ContinueFate/FirstViewController.m
+++ ContinueFate/ContinueFate/FirstViewController.m
@@ -39,20 +39,29 @@
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
 {
-    return 50;
+    return 25;
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
     UIView *headView = [[UIView alloc] init];
-    headView.backgroundColor = [UIColor redColor];
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(15, 0, 100, 20)];
+    headView.backgroundColor = [UIColor grayColor];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(40, 0, 40, 25)];
     label.text = @"雨松MOMO的程序世界";
     label.textColor = [UIColor whiteColor];
     label.textAlignment = NSTextAlignmentCenter;
     label.font = [UIFont fontWithName:[[UIFont familyNames] objectAtIndex:10] size:20];
     label.backgroundColor = [UIColor darkGrayColor];
     [headView addSubview:label];
+    
+    UILabel *label2 = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 25, 25)];
+    
+    label.text = @"";
+    label2.font = [UIFont fontWithName:[[UIFont familyNames] objectAtIndex:10] size:20];
+    label2.backgroundColor = [UIColor colorWithRed:234 green:67 blue:112 alpha:100];
+    
+    
+    [headView addSubview:label2];
     return headView;
 }
 
