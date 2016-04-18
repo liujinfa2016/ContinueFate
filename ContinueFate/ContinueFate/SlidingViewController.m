a1abb4b1a64978731967555a5e7c9dec17cb810a
刘金发 <1152104051@qq.com>
2016-04-18 17:14:06 +0800

    更新

--- ContinueFate/ContinueFate/SlidingViewController.m
+++ ContinueFate/ContinueFate/SlidingViewController.m
@@ -7,16 +7,26 @@
 //
 
 #import "SlidingViewController.h"
+#import "SlidingTableViewCell.h"
 
 
 @interface SlidingViewController ()
-
+@property(strong,nonatomic) NSArray *dict;
+@property(strong,nonatomic) NSArray *imageBrr;
 @end
 
 @implementation SlidingViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UIImage *image1 =[UIImage imageNamed:@"预约咨询"];
+    UIImage *image2 =[UIImage imageNamed:@"预约咨询"];
+    UIImage *image3 =[UIImage imageNamed:@"预约咨询"];
+    UIImage *image4 =[UIImage imageNamed:@"预约咨询"];
+    UIImage *image5 =[UIImage imageNamed:@"预约咨询"];
+    UIImage *image6 =[UIImage imageNamed:@"预约咨询"];
+    _imageBrr =@[image1,image2,image3,image4,image5,image6];
+    _dict = @[@"预约管理",@"我的问答",@"我的收藏",@"我的投稿",@"专家入驻",@"建议反馈" ];
     
 }
 
@@ -24,7 +34,9 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+-(void)slidingTableViewCell{
+  
+}
 /*
 #pragma mark - Navigation
 
@@ -35,12 +47,16 @@
 }
 */
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 6;
+    return _dict.count;
     
 }
 -(UITableViewCell *)tableView:(UITableView *)tableView  cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    SlidingTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    cell.Lable.text=_dict[indexPath.row];
+    cell.image.image =_imageBrr[indexPath.row];
+    
     
     return cell;
 }
