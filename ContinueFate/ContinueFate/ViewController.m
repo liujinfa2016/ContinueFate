3b294b132e735d962f41ec4904ee0300eadc84ef
刘金发 <1152104051@qq.com>
2016-04-22 19:55:58 +0800

    ....

--- ContinueFate/ContinueFate/ViewController.m
+++ ContinueFate/ContinueFate/ViewController.m
@@ -76,7 +76,31 @@
     if (username.length == 0 || password.length == 0) {
         [Utilities popUpAlertViewWithMsg:@"请填写所有信息" andTitle:nil onView:self];
         return;
+        
     }
+    NSLog(@"%@",username);
+    NSLog(@"%@",password);
+    NSDictionary *parameters = @{@"code":username,@"pwd":password,@"loginType":@1};
+    [RequestAPI postURL:@"/login" withParameters:parameters success:^(id responseObject) {
+        NSLog(@"responseObject :%@",responseObject);
+        switch ([responseObject[@"resultFlag"]integerValue]) {
+            case 8001:
+                
+                NSLog(@"登陆成功");
+                
+                break;
+            case 6001:
+                [Utilities popUpAlertViewWithMsg:@"登录失败" andTitle:nil onView:self];
+                break;
+            default:break;
+        }
+        
+    } failure:^(NSError *error) {
+        NSLog(@"error:%@",error.userInfo);
+        NSLog(@"登录失败");
+        
+    }];
+
 //    [self signInWithUsername:username addpassword:password];
 }
 
