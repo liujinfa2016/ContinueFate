a1e274115fbf0a77fee0cbdc76ac580b09c80300
demon <237594006@qq.com>
2016-04-22 10:33:02 +0800

    登录、注册、密码重置 页面更新

--- ContinueFate22/ContinueFate/RegisterViewController.h
+++ ContinueFate22/ContinueFate/RegisterViewController.h
@@ -9,10 +9,13 @@
 #import <UIKit/UIKit.h>//注册页面
 
 @interface RegisterViewController : UIViewController
-@property (weak, nonatomic) IBOutlet UITextField *UsernameTF;
-@property (weak, nonatomic) IBOutlet UITextField *TelTF;
-@property (weak, nonatomic) IBOutlet UITextField *PasswordTF;
-@property (weak, nonatomic) IBOutlet UITextField *PasswordAgainTF;
-- (IBAction)Registered:(UIButton *)sender forEvent:(UIEvent *)event;
+@property (weak, nonatomic) IBOutlet UITextField *UsernameTF;//用户名
+@property (weak, nonatomic) IBOutlet UITextField *TelTF;//手机号
+@property (weak, nonatomic) IBOutlet UITextField *PasswordTF;//密码
+@property (weak, nonatomic) IBOutlet UITextField *PasswordAgainTF;//再次输入密码
+@property (weak, nonatomic) IBOutlet UITextField *registertf;//输入验证码
+- (IBAction)TestGetCodeAction:(UIButton *)sender forEvent:(UIEvent *)event;//获取验证码
+- (IBAction)Registered:(UIButton *)sender forEvent:(UIEvent *)event;//注册
+
 
 @end
---
+++ ContinueFate22/ContinueFate/twoPasswordResetViewController.h
@@ -0,0 +1,17 @@
+//
+//  twoPasswordResetViewController.h
+//  ContinueFate
+//
+//  Created by demon on 16/4/22.
+//  Copyright © 2016年 XuYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface twoPasswordResetViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *TelLable;//我们已将验证码发送至您的手机号：765432654
+@property (weak, nonatomic) IBOutlet UITextField *nPasswordTF;//设置新密码
+@property (weak, nonatomic) IBOutlet UITextField *nPasswordAgainTF;//再次输入新密码
+- (IBAction)confirmAction:(UIButton *)sender forEvent:(UIEvent *)event;//确认修改
+
+@end
