b67c9feaa4862646063eeae8f06dd02fc8a83cfc
刘金发 <1152104051@qq.com>
2016-04-22 20:03:18 +0800

    no message

--- ContinueFate/ContinueFate/ConsultingViewController.m
+++ ContinueFate/ContinueFate/ConsultingViewController.m
@@ -7,15 +7,8 @@
 //
 
 #import "ConsultingViewController.h"
-#import "CExpertsTableViewCell.h"
-#import "CEDetailsViewController.h"
-#import <SDWebImage/UIImageView+WebCache.h>
-#import "ArticleObject.h"
-@interface ConsultingViewController () <UITableViewDataSource,UITableViewDelegate> {
-    NSInteger page;
-    NSInteger perPage;
-}
-@property (strong ,nonatomic)NSMutableArray *objArr;
+
+@interface ConsultingViewController ()
 
 @end
 
@@ -23,13 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [super viewDidLoad];
-    page = 1;
-    perPage = 4;
-    _tableView.dataSource = self;
-    _tableView.delegate = self;
     // Do any additional setup after loading the view.
-    [self requestData];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -37,68 +24,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void) requestData {
-    //NSDictionary *parameters = @{@"name":@"",@"subtype":@"",@"page":@(page),@"perPage":@(perPage)};
-    
-    // 获取请求地址
-    NSString *url = @"http://192.168.61.85:8080/XuYuanProject/expertsList";
-    //  UIActivityIndicatorView *avi = [Utilities getCoverOnView:self.view];
-    //POST请求数据
-    [[AppAPIClient sharedClient] POST:url parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        //请求成功执行以下方法
-        NSLog(@"responseObject = %@",responseObject);
-        //判断请求是否成功
-        if ([responseObject[@"resultFlag"]integerValue] == 8001) {
-            NSDictionary *result = responseObject[@"result"];
-            NSArray *models = result[@"models"];
-            if (page == 1){
-                _objArr = nil;
-                _objArr = [NSMutableArray new];
-                perPage = 4;
-            }
-            //遍历models的内容
-            for (NSDictionary *dic in models) {
-                [_objArr addObject:dic];
-                
-                NSLog(@"dic = %@",_objArr);
-                
-            }
-            //重载表格
-            [self.tableView reloadData];
-            
-            
-        }
-        //请求失败执行以下方法
-    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        NSLog(@"error = %@",error.description);
-    }];
-}
-
-
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return _objArr.count;
-}
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    CExpertsTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
-    NSDictionary *dict = _objArr[indexPath.row];
-    
-    cell.Username.text = [NSString stringWithFormat:@"%@", dict[@"name"]];
-    cell.Edetail.text = [NSString stringWithFormat:@"%@", dict[@"expertlv"]];
-    cell.ReadingN.text= [NSString stringWithFormat:@"%@", dict[@"orderCount"]];
-    NSURL *photoUrl = [NSURL URLWithString:dict[@"headimage"]];
-    //结合SDWebImage通过图片路径来实现异步加载和缓存（本案中加载到一个图片视图中）
-    [cell.image sd_setImageWithURL:photoUrl placeholderImage:[UIImage imageNamed:@"初始头像"]];
-    return cell;
-}
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    CEDetailsViewController *date = [Utilities getStoryboardInstanceByIdentity:@"Consulting" byIdentity:@"EDetails"];
-    date.hidesBottomBarWhenPushed = YES;
-    NSDictionary *dict = _objArr[indexPath.row];
-    date.dict = dict;
-    [self.navigationController pushViewController:date animated:YES];
-}
 /*
 #pragma mark - Navigation
 
