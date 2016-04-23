e197f25840e43a2725bb0cd946158fb7f06adce4
hua <1174157690@qq.com>
2016-04-23 17:48:01 +0800

    Question

--- ContinueFate/ContinueFate/QAskViewController.m
+++ ContinueFate/ContinueFate/QAskViewController.m
@@ -9,6 +9,8 @@
 #import "QAskViewController.h"
 
 @interface QAskViewController ()
+@property (strong,nonatomic) UILabel *titleLbl;
+@property (strong,nonatomic) UILabel *substance;
 
 @end
 
@@ -17,13 +19,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _titleView.delegate = self;
+    _substanceView.delegate = self;
+    _titleLbl = [[UILabel alloc]initWithFrame:CGRectMake(3, 3, 200, 20)];
+    _titleLbl.enabled = NO;
+    _titleLbl.text = @"请输入标题";
+    _titleLbl.font = [UIFont systemFontOfSize:13];
+    _titleLbl.textColor = [UIColor lightGrayColor];
+    [_titleView addSubview:_titleLbl];
+    
+    
+    _substance = [[UILabel alloc]initWithFrame:CGRectMake(3, 3, 300, 20)];
+    _substance.enabled = NO;
+    _substance.text = @"请详细写出您的问题和感受(不少于60个字)";
+    
+    _substance.font = [UIFont systemFontOfSize:13];
+    _substance.textColor = [UIColor lightGrayColor];
+    [_substanceView addSubview:_substance];
     
  }
+
+- (void)textViewDidChange:(UITextView *)textView{
+    
+    if ([_substanceView.text length] == 0) {
+        _substance.hidden = NO;
+    }else{
+        _substance.hidden = YES;
+    }
+    
+    if ([_titleView.text length] == 0) {
+        _titleLbl.hidden = NO;
+    }else{
+        _titleLbl.hidden = YES;
+    }
+    
+}
+
 - (IBAction)cancelAction:(UIBarButtonItem *)sender {
-    [self dismissViewControllerAnimated:NO completion:nil];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (IBAction)saveAction:(UIBarButtonItem *)sender {
-    [self dismissViewControllerAnimated:NO completion:nil];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 @end
--- ContinueFate/ContinueFate/QuestionObject.m
+++ ContinueFate/ContinueFate/QuestionObject.m
@@ -1,30 +1,25 @@
 //
-//  ArticleObject.m
+//  QuestionObject.m
 //  ContinueFate
 //
-//  Created by jdld on 16/4/18.
+//  Created by hua on 16/4/23.
 //  Copyright © 2016年 XuYuan. All rights reserved.
 //
 
-#import "ArticleObject.h"
+#import "QuestionObject.h"
 
-@implementation ArticleObject
-
+@implementation QuestionObject
 - (id)initWithDictionary:(NSDictionary *)dict {
     self = [super init];
-    
     self.id = [dict[@"id"]isKindOfClass:[NSNull class]]?@"":dict[@"id"];
-    self.number = [dict[@"number"]isKindOfClass:[NSNull class]?0:dict[@"number"]];
     self.titlename = [dict[@"titlename"]isKindOfClass:[NSNull class]]?@"":dict[@"titlename"];
     self.substance = [dict[@"substance"]isKindOfClass:[NSNull class]]?@"":dict[@"substance"];
-    self.edittime = [dict[@"edittime"]isKindOfClass:[NSNull class]]?@"":dict[@"edittime"];
     self.time = [dict[@"time"]isKindOfClass:[NSNull class]]?@"":dict[@"time"];
     self.hits = [dict[@"hits"]isKindOfClass:[NSNull class]?0:dict[@"hits"]];
     self.type = [dict[@"type"]isKindOfClass:[NSNull class]]?@"":dict[@"type"];
-    self.usertype = [dict[@"usertype"]isKindOfClass:[NSNull class]?0:dict[@"usertype"]];
-    self.username = [dict[@"username"]isKindOfClass:[NSNull class]]?@"":dict[@"username"];
     self.name = [dict[@"name"]isKindOfClass:[NSNull class]]?@"":dict[@"name"];
     return self;
 }
+
 
 @end
