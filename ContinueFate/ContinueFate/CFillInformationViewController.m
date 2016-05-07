0c9050c7655c0372efc5a5766199d7d286e44e19
demon <237594006@qq.com>
2016-05-07 14:53:08 +0800

    咨询页面更新

--- ContinueFate/ContinueFate/CFillInformationViewController.m
+++ ContinueFate/ContinueFate/CFillInformationViewController.m
@@ -47,6 +47,7 @@
     [self requestData];
     [self creatDate];
     [self grenderTFC];
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"导航条"] forBarMetrics:UIBarMetricsDefault];
 }
 - (void) grenderTFC {
     _grenderView = [[UIPickerView alloc] init];
