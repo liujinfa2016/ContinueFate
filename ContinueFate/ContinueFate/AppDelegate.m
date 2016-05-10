b21ae4eb13c84b2e0c3b9f816469c0db0f1f480c
liujinfa <liujinfa@suguang.local>
2016-05-10 20:25:07 +0800

    11111

--- ContinueFate/ContinueFate/AppDelegate.m
+++ ContinueFate/ContinueFate/AppDelegate.m
@@ -10,6 +10,7 @@
 #import "TabBarViewController.h"
 #import "SlidingViewController.h"
 #import <SMS_SDK/SMSSDK.h>
+
 @interface AppDelegate ()
 @property(strong, nonatomic)ECSlidingViewController *sligingVC;
 
@@ -160,6 +161,31 @@
             abort();
         }
     }
+}
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    
+    if ([url.host isEqualToString:@"safepay"]) {
+        //跳转支付宝钱包进行支付，处理支付结果
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            NSLog(@"result = %@",resultDic);
+        }];
+    }
+    return YES;
+}
+
+// NOTE: 9.0以后使用新API接口
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options
+{
+    if ([url.host isEqualToString:@"safepay"]) {
+        //跳转支付宝钱包进行支付，处理支付结果
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            NSLog(@"result = %@",resultDic);
+        }];
+    }
+    return YES;
 }
 
 @end
